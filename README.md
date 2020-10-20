46f8d5521aae756adbcfd17d60e3f8cd6d6fa838
Samuel Pierre Louis <samuelpierrelouis10@gmail.com>
2020-10-19 20:20:23 -0400

    First TRON

---
+++ README.md
@@ -0,0 +1,24 @@
+### TRON Game - By Samuel Pierre Louis
+### This Gsme is a Project making the game Tron
+
+## Live wireframe
+
+
+# Tasks:
+[✅] Create Folder
+[✅] Create index readme
+[✅] Create styles.css
+[✅] Create scripts.js
+[✅] Create assets folder
+[✅] upload figma outline
+[] Make arena from canvas(background color with height border)
+[] Make player one show on screen
+[] Make player one move forward
+[] Make move Left
+[] Make move Right
+[] Make move down
+[] Make players leave a trail behind
+[] Trail collisoin between player, walls and trail
+[] Repeat for player 2
+
+![fimga](./assets/Group1.jpg)
---
+++ scripts.js
@@ -0,0 +1,135 @@
+let canvas = document.getElementById("Retro");
+let ctx = canvas.getContext("2d");
+// setting our x & y direction
+let dx = 1;
+let dy = 1;
+// setting our starting x & y coordinates 
+let x = canvas.width + 10;
+let y = canvas.height/2;
+let rideWidth = 40;
+let rideHeight = 40;
+// define a char/bikerider
+let player = { 
+    orientation: 'up',
+    height: 40,
+    width: 40, 
+    x: canvas.height/2 - 15, 
+    y: canvas.width/2 - 15
+
+}
+player.img = new Image();
+player.img.src = './assets/tronredbikeup.png'; //Bike image that will move 
+// Trail object
+
+
+
+// Checking for collision
+function has_game_ended()
+
+{  
+  for (let i = 4; i < player.length; i++)
+  {    
+      console.log("checking...")
+    let has_collided = player[i] === player[0] && player[i] === player[0]
+    if (has_collided) 
+      return alert('Gameover')
+}
+  let hitLeftWall = player.x < 0;  
+  let hitRightWall = player.x >= canvas.width - 35;
+  let hitToptWall = player.y <= 0;
+  let hitBottomWall = player.y >= canvas.height - 35;
+    const onGameOver = () => {
+        alert('Game Over');
+        player.x = canvas.height/2 - 15
+        player.y = canvas.width/2 - 15
+        rightPressed = false;
+        leftPressed = false;
+        upPressed = false;
+        downPressed = false;
+    }
+  return hitLeftWall ||  hitRightWall || hitToptWall || hitBottomWall ? onGameOver() : ""
+}
+
+
+// letting page know the buttons ARENT being pressed yet
+let rightPressed = false;
+let leftPressed = false;
+let upPressed = false;
+let downPressed = false;
+
+
+document.onkeydown = keyDownHandler
+
+//if player x and y moves draw trail
+
+function trail() {
+
+}
+
+// defining functions to handle key up & down
+function keyDownHandler(t){
+     rightPressed = false;
+     leftPressed = false;
+     upPressed = false;
+     downPressed = false;
+
+    if(t.key == "Right" || t.key == "ArrowRight"){
+        rightPressed = true 
+        player.orientation = 'right'
+    } else if(t.key == "Left" || t.key == "ArrowLeft"){
+        leftPressed = true;
+        player.orientation = 'left'
+    } else if(t.key == "Up" || t.key == "ArrowUp"){
+        upPressed = true;
+        player.orientation = 'up'
+    } else if(t.key == "Down" || t.key == "ArrowDown"){
+        downPressed = true;
+        player.orientation = 'down'
+    }
+    player.img.src = `./assets/tronredbike${player.orientation}.png`
+
+}
+
+
+// create our character/Bikerider
+function drawChar(){
+    
+    ctx.drawImage(player.img, player.x, player.y, player.height, player.width); 
+    
+}
+
+function moveChar(){
+
+    if(rightPressed){
+        player.x += 4;
+    } else if(leftPressed){
+        player.x -= 4;
+    } else if(upPressed){
+        player.y -= 4;
+    } else if(downPressed){
+        player.y += 4;
+    }
+}
+
+
+
+
+
+
+function draw(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+   moveChar();
+   has_game_ended();
+    drawChar();
+    
+    
+    
+
+  
+    
+}
+
+setInterval(draw, 30)
+
+
+  
